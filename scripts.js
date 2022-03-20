b8bc8f69b9b710206eb96e1f00109d83a271180b
Miti <101258918+MitiBsq@users.noreply.github.com>
2022-03-20 14:00:15 +0200

    Update v2(20/3)

--- scripts.js
+++ scripts.js
@@ -2,20 +2,24 @@
 document.getElementById('game').style.display = 'none';
 document.getElementById('selectDificulty').style.display = 'none';
 document.getElementById('startGame').addEventListener('click', chooseDificulty);
-
+let goodWord=0;
+let theWord;
 //Function for choosing the beginner/advanced levels of the game
 function chooseDificulty() {
     document.getElementById('startGamePlace').remove();
     document.getElementById('selectDificulty').style.display = 'initial';
-    let selectDificultyButton = document.getElementById('selectDificultyButton');
-    selectDificultyButton.addEventListener('click', selectedOption);
+    document.getElementById('selectDificultyButton').addEventListener('click', selectedOption);
 }
 
 /* const beginnerWords=['SUBWAY', 'CLOTHES', 'FUNNY', 'WORD', 'WATER' ];
 const advancedWords=['SYNDROME', 'JOGGING', 'MICROWAVE', 'FLAPJACK', 'MEGAHERTZ']; 
 for (let i = 0 ; i < advancedWords.length ; i++) {
-    let beginner = "advanced" + i;
-    localStorage.setItem(beginner,advancedWords[i]);
+    let beginner = "beginner" + i;
+    localStorage.setItem(beginner,beginnerWords[i]);
+} 
+for (let i = 0 ; i < advancedWords.length ; i++) {
+    let advanced = "advanced" + i;
+    localStorage.setItem(advancerd,advancedWords[i]);
 } */
 
 //Sa lucrez la asta
@@ -29,26 +33,24 @@ function selectedOption() {
     }
 }
 
-
+//Function for generating the game interface
 function generateTheGame() {
     const hearts = new Array();
     for (let i = 0; i < 7; i++) {
         hearts[i] = document.createElement('img');
         hearts[i].src = 'images/heart.png';
-        //Poate sa fac un id la fieacare pt a da remove!
         document.getElementById('totalLifes').appendChild(hearts[i]);
     }
-    const letters = new Array();
-    const theWord = generateTheWord(letters);
-    generateTheKeyboard(letters, theWord);
-    document.getElementById('infoText').innerText = "Your word has " + letters.length + " letters";
+    generateTheWord();
+    generateTheKeyboard();
+    infoTextFunction("Your word has " + theWord.length + " letters");
 }
 
 //Function for generating the word
-function generateTheWord(letters) {
-    const randomWordGenerator = document.getElementById('inputGroupSelect').value + Math.floor(Math.random() * 5);
-    const randomWord = localStorage.getItem(randomWordGenerator);
-    for (let i = 0; i < randomWord.length; i++) {
+function generateTheWord() {
+    theWord = localStorage.getItem(document.getElementById('inputGroupSelect').value + Math.floor(Math.random() * 5));
+    const letters = new Array();
+    for (let i = 0; i < theWord.length; i++) {
         letters[i] = document.createElement('input');
         letters[i].type = 'text';
         letters[i].maxLength = '1';
@@ -56,42 +58,108 @@ function generateTheWord(letters) {
         letters[i].id = 'theLetter' + i;
         document.getElementById('putTheLetters').appendChild(letters[i]);
     }
-    console.log(randomWord)
-    return randomWord;
 }
 
-function generateTheKeyboard(letters, theWord) {
+function generateTheKeyboard() {
     const keyboardLetters = new Array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
     const keyboard = new Array();
     for (let i = 0; i < keyboardLetters.length; i++) {
         keyboard[i] = document.createElement('button');
         keyboard[i].innerText = keyboardLetters[i];
-        keyboard[i].id = "keyboardLetter";
+        keyboard[i].className = "keyboardLetter";
+        keyboard[i].id = keyboardLetters[i];
         keyboard[i].addEventListener('click', () => {
-            checkTheWord(keyboard[i].innerText, letters, theWord)
+            checkTheWord(keyboard[i].innerText)
         });
         document.getElementById('keyboard').appendChild(keyboard[i]);
     }
 }
 
-
-//Sa fac sa numar cuvintele exemplu goodWord =1 goodWord++ si cand ajunge la limita sa bag functia de sfarsit joc
 //Function for analysing the letter choosed by the player
-function checkTheWord(letterPressed, letters, theWord) {
+function checkTheWord(letterPressed) {
     let check=false;
     for (let i = 0; i < theWord.length; i++) {
         if (letterPressed === theWord[i]) {
-            console.log(theWord[i])
-            letters[i].value = letterPressed;
-            letters[i].disabled = true;
+            document.getElementById('theLetter' + i).value=letterPressed;
+            document.getElementById('theLetter' + i).disabled=true;
+            keyPressedEdit(letterPressed, 'yellowgreen');
             check=true;
+            ++goodWord;
         }
     }
     if(check===false) {
         document.getElementById('totalLifes').removeChild(document.getElementById('totalLifes').lastChild);
+        keyPressedEdit(letterPressed, 'crimson');
+    }
+    analyseTheSituation();
+}
+
+function analyseTheSituation() {
+    if (document.getElementById('totalLifes').childNodes.length===0) {
+        infoTextFunction("Saddly, You've lost!\n\n" + "You're word was:");
+        showResults();
+        setTimeout(function () { restartButton() }, 1.0*2000);
+    } else {
+        if (goodWord === theWord.length) {
+            if (document.getElementById('totalLifes').childNodes.length===7) {
+                infoTextFunction("Congratulation! You've done a PERFECT SCORE by winning without losing a single heart!");
+                showResults();
+                setTimeout(function () { restartButton() }, 1.0*2000);
+            } else {
+                infoTextFunction("Congratulation! You've won!");
+                showResults();
+                setTimeout(function () { restartButton() }, 1.0*2000);
+            }
+        } else {
+            if (goodWord > parseInt(theWord.length/2 )) {
+                infoTextFunction("You have only " + (theWord.length - goodWord)+ " letters left");
+            }
+        }
     }
 }
 
+function keyPressedEdit(letterPressed, color) {
+    document.getElementById(letterPressed).disabled=true;
+    document.getElementById(letterPressed).style.backgroundColor=color;
+}
 
+function infoTextFunction(desiredText) {
+    document.getElementById('infoText').innerText = desiredText;
+}
 
-//Sa fac jocul si partea in care scad inimile
+ function showResults() {
+    document.getElementById('keyboard').style.visibility='hidden';
+    document.getElementById('totalLifes').style.visibility='hidden';
+    for (let i = 0; i < theWord.length; i++) {
+        document.getElementById('theLetter' + i).value=theWord[i];
+        document.getElementById('theLetter' + i).disabled=true;
+    }
+} 
+
+function restartButton() {
+    const resetButton=document.createElement('button');
+    resetButton.innerText="Restart Game";
+    resetButton.className='btn btn-dark btn-lg';
+    resetButton.id='resetButton';
+    document.getElementById('resetButtonPlace').appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame );
+}
+
+function resetGame() {
+    document.getElementById('resetButton').remove();
+    document.getElementById('selectDificulty').style.display = 'initial';
+    document.getElementById('totalLifes').style.visibility='visible';
+    document.getElementById('keyboard').style.visibility='visible';
+    const keyboardLetters = new Array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    for (let i = 0; i < keyboardLetters.length; i++) {
+        document.getElementById( keyboardLetters[i]).remove();
+    }
+    infoTextFunction('');
+    for (let i = 0; i < theWord.length; i++) {
+        document.getElementById('theLetter' + i).remove();
+    }  
+    document.getElementById('selectDificultyButton').addEventListener('click', selectedOption);
+}
+
+//Sa vad de partea de joc in 2 playeri 
+//Sa fac chestia daca scrii manual litera in input
